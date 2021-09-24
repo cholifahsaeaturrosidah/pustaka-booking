661b677426ee4fef1a3b6a9e7faa54f7a6a3fdfd
cholifahsaeaturrosidah <cholifahsaeaturrosidah.cholifahsaeatur@gmail.com>
2021-09-25 00:38:07 +0700

    first commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>perkenalkan</h1>";
+        echo"Nama saya Cholifah Saeaturrosidah
+            Saya tinggal di daerah Yogyakarta
+            olahraga yang saya sukai adalah Bulutangkis";
+    }
+}    
